3b28744e3240d0ea8f62c12dda0da434a17942f8
Pravin Babar <pravinbabar9224@gmail.com>
2020-05-17 15:24:05 +0530

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -18,6 +18,6 @@ public class GreetingController {
     }
     @RequestMapping("/")
     public String defualtgreeting(){
-	return("Hello!!! Welcome to Spring boot application into Kubernates");
+	return("Hello!!! Welcome to Spring boot application into k8s");
 	}
 }
