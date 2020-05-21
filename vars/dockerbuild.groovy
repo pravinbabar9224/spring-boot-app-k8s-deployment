2afb74d4e74b76b1de8e159b37ab5c2cc596e3db
Pravin Babar <pravinbabar9224@gmail.com>
2020-05-21 12:03:50 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,12 +1,12 @@
-def call(String hubuser, String repo, String repotag) {
+def call(String hubuser, String repo) {
     withCredentials([usernamePassword(
-            credentialsId: "Dockerhub",
+            credentialsId: "docker-hub",
             usernameVariable: "Username",
             passwordVariable: "Password"
     )]) {
         sh "docker login -u '$Username' -p '$Password'"
     }
-    sh "docker image build -t ${hubuser}/${repo}:${repotag}-v${env.BUILD_NUMBER} .  --no-cache"
-    sh "docker image push ${hubuser}/${repo}:${repotag}-v${env.BUILD_NUMBER}"
-    sh "docker image rm ${hubuser}/${repo}:${repotag}-v${env.BUILD_NUMBER}"   
+    sh "docker image build -t ${hubuser}/${repo}:v${env.BUILD_NUMBER} .  --no-cache"
+    sh "docker image push ${hubuser}/${repo}:v${env.BUILD_NUMBER}"
+    sh "docker image rm ${hubuser}/${repo}:v${env.BUILD_NUMBER}"   
 }
