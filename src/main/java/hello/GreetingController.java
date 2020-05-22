1198dae40a686731da9eda55e4096c9a106b266a
Pravin Babar <pravinbabar9224@gmail.com>
2020-05-22 11:28:32 +0530

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -18,6 +18,6 @@ public class GreetingController {
     }
     @RequestMapping("/")
     public String defualtgreeting(){
-	return("Hello!!! Welcome to Spring boot application in Pravin's EKS");
+	return("Hello!!! Welcome to Spring boot application in Docker");
 	}
 }
