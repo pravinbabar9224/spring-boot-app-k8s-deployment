fba4cd048ae4be0a621e2d89ab0ce13912de4eb8
Pravin Babar <pravinbabar9224@gmail.com>
2020-05-22 13:34:08 +0530

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -18,6 +18,6 @@ public class GreetingController {
     }
     @RequestMapping("/")
     public String defualtgreeting(){
-	return("Hello!!! Welcome to Spring boot application in AWS EKS Cluster");
+	return("Hello!!! Welcome to Spring boot application in AWS");
 	}
 }
