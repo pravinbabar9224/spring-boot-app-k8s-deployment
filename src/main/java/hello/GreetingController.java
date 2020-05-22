eb1ae7dd98e59a831302a2b97dcf3f7ba61c9a6c
Pravin Babar <pravinbabar9224@gmail.com>
2020-05-22 16:44:24 +0530

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -18,6 +18,6 @@ public class GreetingController {
     }
     @RequestMapping("/")
     public String defualtgreeting(){
-	return("Hello!!! Welcome to Spring Boot lskfsdlfksdglkb");
+	return("Hello!!! Welcome to Spring Boot  sglj ljg sdljg ");
 	}
 }
